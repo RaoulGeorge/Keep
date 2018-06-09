4c4f3bdfcf6a95f86ba2848759426949f1da7e11
Raoul George <raoulgeorge@gmail.com>
2018-06-09 12:07:11 +0530

    define handleChange

--- keep/src/js/components/Note.js
+++ keep/src/js/components/Note.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 const mapStateToProps = (state, ownProps) => {
 	return { note: state.notes.filter(note=> { return note.id == ownProps.details.id})[0] };
 };
 
-const ConnectedNote = ({note}) => (
-	<div>
-		<input
-			type="text"
-			className="form-control"
-			id="text"
-			value={note.details}
-			// onChange={this.handleChange}
-		/>
-	</div>
-	
-);
+class ConnectedNote extends Component {
+	constructor() {
+		super();
+		this.handleChange = this.handleChange.bind(this);
+	}
 
+	handleChange() {}
+
+	render() {
+		return (
+			<div>
+				<input
+					type="text"
+					className="form-control"
+					id="text"
+					value={this.props.note.details}
+					onChange={this.handleChange}
+				/>
+			</div>
+		);
+	}
+}
+	
 const Note = connect(mapStateToProps)(ConnectedNote);
 
 export default Note;
